0c864e36b46912304ffc22c759b3668dd73e7273
Barra Malik Wibowo <malikbarra@gmail.com>
2022-09-18 15:14:41 +0700

    Calculator Luas

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Rumus Persegi Panjang
+from re import L
+from tkinter import Variable
+print("Rumus Persegi Panjang")
+P = int(input("\nInput Panjang : "))
+L = int(input("\nInput Lebar : "))
+Luas_Persegi_Panjang = P * L
+Keliling_Persegi_Panjang = 2 * (P + L)
+print("Luas Persegi Panjang\t : ",Luas_Persegi_Panjang)
+print("Keliling Persegi Panjang\t : ",Keliling_Persegi_Panjang)
