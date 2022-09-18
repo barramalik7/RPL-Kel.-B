5bb55d4471a4d540f9670c3f9ccaafbb1c06d899
Selezia <111285108+SleepyMarmar@users.noreply.github.com>
2022-09-18 16:38:31 +0700

    Rumus

--- calculator.py
+++ calculator.py
@@ -8,3 +8,11 @@ Luas_Persegi_Panjang = P * L
 Keliling_Persegi_Panjang = 2 * (P + L)
 print("Luas Persegi Panjang\t : ",Luas_Persegi_Panjang)
 print("Keliling Persegi Panjang\t : ",Keliling_Persegi_Panjang)
+
+# Rumus Lingkaran
+print("\nRumus Lingkaran")
+R = int(input("\nInput Jari-jari :"))
+Luas_Lingkaran = 3.14 * R * R
+Keliling_Lingkaran = 2 * 3.14 * R
+print("Luas Lingkaran\t :",Luas_Lingkaran)
+print("Keliling Lingkaran\t :",Keliling_Lingkaran)
